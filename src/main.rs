4f86d2aabc560d315f1b8893fac4160c415db994
KPMGE <jesuskevin254@gmail.com>
2022-12-18 18:52:30 -0300

    :sparkles: add first full-working implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+// AABBGGRR
+use std::fs::File;
+use std::io::prelude::*;
+
+#[derive(Debug)]
+struct Pixel {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+fn fill_image(pixels: &mut Vec<Pixel>, size: usize, color: u32) {
+    for _ in 0..size {
+        pixels.push(Pixel {
+            r: (color & 0xff) as u8,
+            g: ((color >> 8) & 0xff) as u8,
+            b: ((color >> 16) & 0xff) as u8,
+        })
+    }
+}
+
+fn save_image_as_ppm(file_path: &str, pixels: &Vec<Pixel>, width: usize, height: usize) {
+    let mut file = File::create(file_path).unwrap();
+
+    if let Err(e) = write!(file, "P6\n{} {} 255\n", width, height) {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+
+    for pixel in pixels {
+        let bytes: [u8; 3] = [pixel.r, pixel.g, pixel.b];
+        if let Err(e) = file.write_all(&bytes) {
+            eprintln!("Couldn't write to file: {}", e);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    const WIDTH: usize = 600;
+    const HEIGHT: usize = 400;
+    const IMAGE_SIZE: usize = WIDTH * HEIGHT;
+    let mut pixels: Vec<Pixel> = Vec::with_capacity(IMAGE_SIZE);
+
+    fill_image(&mut pixels, IMAGE_SIZE, 0x00ff);
+    save_image_as_ppm("out.ppm", &pixels, WIDTH, HEIGHT);
 }
